207b3eb004a9db1130ffd6568ee954416564e1a4
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-21 20:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+let divs = document.querySelectorAll(".image");
+let draggeddiv = null;
+
+divs.forEach((div) => {
+    div.setAttribute("draggable", "true");
+    div.addEventListener("dragstart", (event) => {
+        draggeddiv = event.target;
+    });
+    div.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+    div.addEventListener("drop", (event) => {
+        event.preventDefault();
+        let temp = draggeddiv.id;
+        draggeddiv.id = event.target.id;
+        event.target.id = temp;
+    });
+});
